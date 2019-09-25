8b609d54c9b0f1e07e875d54053b0b474b97d3c3
Koji Shigemura <shige0501@gmail.com>
2019-09-25 22:30:37 +0900

    ListViewの表示追加

--- app/src/main/java/net/buildbox/sample/buildbox_listview/MainActivity.kt
+++ app/src/main/java/net/buildbox/sample/buildbox_listview/MainActivity.kt
@@ -2,11 +2,26 @@ package net.buildbox.sample.buildbox_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeListView()
+    }
+
+    private fun initializeListView() {
+            val language = listOf("フシギダネ", "フシギソウ", "フシギバナ",
+                "メガフシギバナ", "ヒトカゲ", "リザード", "リザードン", "メガリザードンX",
+                "メガリザードンY", "ゼニガメ", "カメール", "カメックス", "メガカメックス", "キャタピー",
+                "トランセル", "バタフリー", "メガスピアー", "ポッポ")
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, language)
+
+        val listView: ListView = findViewById(R.id.list_view)
+        listView.adapter = adapter
     }
 }
